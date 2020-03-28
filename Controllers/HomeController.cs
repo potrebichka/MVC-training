f1dd5248e47dec41fa4b11484f7d966745e9e0e4
Nina Potrebich <potrebich@gmail.com>
2020-03-27 21:37:31 -0700

    Play with ContentResult

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,5 +22,27 @@ namespace MVC1.Controllers
         {
             return View();
         }
+        public ActionResult GetEmpName(int EmpId)
+        {
+            var employees = new[] {
+                new {EmpId = 1, EmpName = "Scott", Salary = 8000},
+                new {EmpId = 2, EmpName = "Smith", Salary = 2540},
+                new {EmpId = 3, EmpName = "Allen", Salary = 9400}
+            };
+            string matchEmpName = null;
+            foreach (var item in employees)
+            {
+                if (item.EmpId == EmpId)
+                {
+                    matchEmpName = item.EmpName;
+                }
+            }
+            //return new ContentResult() { Content = matchEmpName, ContentType = "text/plain"};
+            return Content(matchEmpName, "text/plain");
+        }
+        public ActionResult GetPaySlip(int EmpId)
+        {
+            return View();
+        }
     }
 }
